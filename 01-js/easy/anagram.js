97fe1153f0f6f38bfe31ec378c29c4b61a836141
perpetual-cosmos <prataptarunsingh241@gmail.com>
2024-09-25 21:01:48 +0530

    anagaram solution

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,18 +5,21 @@
 */
 
 function isAnagram(str1, str2) {
-  let s1=new Array.apply(0,Array(26));
-  let s2=new Array.apply(0,Array(26));
-  for(let i=0;i<str1.length;i++){
-    s1[str1.charCodeAt(i)-97]++;
+  if (str1.length !== str2.length) 
+    return false;  // Early length check
+
+  let s1 = new Array(26).fill(0);
+  let s2 = new Array(26).fill(0);
+
+  for (let i = 0; i < str1.length; i++) {
+    s1[str1.charCodeAt(i) - 97]++;
+    s2[str2.charCodeAt(i) - 97]++;
   }
-  for(let i=0;i<str2.length;i++){
-    s2[str2.charCodeAt(i)-97]++;
+
+  for (let i = 0; i < 26; i++) {
+    if (s1[i] !== s2[i]) return false;
   }
-  for(let i=0;i<26;i++){
-    if(s1[i]!=s2[i])
-      return false;
-  }
+
   return true;
 }
 
